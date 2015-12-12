5cadc725a120b14b56b3db85a5c4766881f496a3
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-12 13:50:48 +0100

    Add initial recipe

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'dotjanus'
+maintainer       'Marcin Nowicki'
+maintainer_email 'marcin@prodix.pl'
+license          'MIT'
+description      'Installs janus customisation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
